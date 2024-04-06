af1e5262ba1923ba5fd89c4eea1e697b93f0c8b2
sas-miles <132398659+sas-miles@users.noreply.github.com>
2024-04-06 13:36:09 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@src": resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react()],
-})
+});
