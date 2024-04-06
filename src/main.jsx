edf09aa3342937aa7c8e67a73377139503322497
sas-miles <132398659+sas-miles@users.noreply.github.com>
2024-04-06 14:16:24 -0400

    modelsrc in index

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+const rootElement = document.getElementById("root");
+const modelSrc = rootElement.getAttribute("data-model");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App modelSrc={modelSrc} />
