326a148b0a06554df75ba94a76b2bc0b38db302f
sas-miles <132398659+sas-miles@users.noreply.github.com>
2024-04-06 14:58:36 -0400

    new modelsrc file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
-const modelSrc = rootElement.getAttribute("data-model");
+const modelSrc = rootElement.dataset.model || null;
+
+if (modelSrc) {
+  // Initializes the main React element and preloads the GLTF.
+  const root = createRoot(rootElement);
+  root.render(<App model={modelSrc} />);
+  useGLTF.preload(modelSrc);
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
